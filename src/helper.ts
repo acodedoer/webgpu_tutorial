4e25b4fd91ae4a00bbc6be5c7e7ae33fd65a49e1
acodedoer <misbahu.s.z@gmail.com>
2022-05-26 15:58:41 +0100

    not working

--- src/helper.ts
+++ src/helper.ts
@@ -4,17 +4,17 @@ export const CheckWebGPU = () => {
         result = `Your current browser does not support WebGPU!.`;
     } 
 
-    const canvas = <HTMLCanvasElement>document.getElementById('canvas-webgpu');
-    if(canvas){
-        const div = <HTMLDivElement>document.getElementsByClassName('item2')[0];
-        canvas.width  = div.offsetWidth;
-        canvas.height = div.offsetHeight;
+    // const canvas = <HTMLCanvasElement>document.getElementById('canvas-webgpu');
+    // if(canvas){
+    //     const div = <HTMLDivElement>document.getElementsByClassName('item2')[0];
+    //     canvas.width  = div.offsetWidth;
+    //     canvas.height = div.offsetHeight;
 
-        function windowResize() {
-            canvas.width  = div.offsetWidth;
-            canvas.height = div.offsetHeight;
-        };
-        window.addEventListener('resize', windowResize);
-    }
+    //     function windowResize() {
+    //         canvas.width  = div.offsetWidth;
+    //         canvas.height = div.offsetHeight;
+    //     };
+    //     window.addEventListener('resize', windowResize);
+    // }
     return result;
 }
